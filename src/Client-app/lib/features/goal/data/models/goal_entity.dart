eb51f4c53017a355ac486b2f9ca57d17fbc275bc
quangtadd <tadd1632004@gmail.com>
2026-08-11 18:12:48 +0700

    feat(goal): create GoalEntity model mapping to Drift Goals table

--- src/Client-app/lib/features/goal/data/models/goal_entity.dart
+++ src/Client-app/lib/features/goal/data/models/goal_entity.dart
@@ -1,8 +1,7 @@
-import 'package:equatable/equatable.dart';
 import 'package:drift/drift.dart';
 import '../../../../core/database/app_database.dart';
 
-class GoalEntity extends Equatable {
+class GoalEntity {
   final String id;
   final int idaccount;
   final String name;
@@ -17,7 +16,7 @@ class GoalEntity extends Equatable {
   final String syncStatus;
   final DateTime updatedAt;
 
-  const GoalEntity({
+  GoalEntity({
     required this.id,
     required this.idaccount,
     required this.name,
@@ -33,56 +32,71 @@ class GoalEntity extends Equatable {
     required this.updatedAt,
   });
 
-  factory GoalEntity.fromDrift(Goal data) {
+  factory GoalEntity.fromDrift(Goal d) {
     return GoalEntity(
-      id: data.id,
-      idaccount: data.idaccount,
-      name: data.name,
-      targetAmount: data.targetAmount,
-      currentAmount: data.currentAmount,
-      targetDate: data.targetDate,
-      icon: data.icon,
-      colour: data.colour,
-      note: data.note,
-      isCompleted: data.isCompleted,
-      isDeleted: data.isDeleted,
-      syncStatus: data.syncStatus,
-      updatedAt: data.updatedAt,
+      id: d.id,
+      idaccount: d.idaccount,
+      name: d.name,
+      targetAmount: d.targetAmount,
+      currentAmount: d.currentAmount,
+      targetDate: d.targetDate,
+      icon: d.icon,
+      colour: d.colour,
+      note: d.note,
+      isCompleted: d.isCompleted,
+      isDeleted: d.isDeleted,
+      syncStatus: d.syncStatus,
+      updatedAt: d.updatedAt,
     );
   }
 
   GoalsCompanion toCompanion() {
-    return GoalsCompanion(
-      id: Value(id),
-      idaccount: Value(idaccount),
-      name: Value(name),
-      targetAmount: Value(targetAmount),
+    return GoalsCompanion.insert(
+      id: id,
+      idaccount: idaccount,
+      name: name,
+      targetAmount: targetAmount,
       currentAmount: Value(currentAmount),
-      targetDate: Value(targetDate),
+      targetDate: targetDate,
       icon: Value(icon),
       colour: Value(colour),
       note: Value(note),
       isCompleted: Value(isCompleted),
       isDeleted: Value(isDeleted),
       syncStatus: Value(syncStatus),
-      updatedAt: Value(updatedAt),
+      updatedAt: updatedAt,
     );
   }
 
-  @override
-  List<Object?> get props => [
-        id,
-        idaccount,
-        name,
-        targetAmount,
-        currentAmount,
-        targetDate,
-        icon,
-        colour,
-        note,
-        isCompleted,
-        isDeleted,
-        syncStatus,
-        updatedAt,
-      ];
+  GoalEntity copyWith({
+    String? id,
+    int? idaccount,
+    String? name,
+    double? targetAmount,
+    double? currentAmount,
+    DateTime? targetDate,
+    String? icon,
+    String? colour,
+    String? note,
+    bool? isCompleted,
+    bool? isDeleted,
+    String? syncStatus,
+    DateTime? updatedAt,
+  }) {
+    return GoalEntity(
+      id: id ?? this.id,
+      idaccount: idaccount ?? this.idaccount,
+      name: name ?? this.name,
+      targetAmount: targetAmount ?? this.targetAmount,
+      currentAmount: currentAmount ?? this.currentAmount,
+      targetDate: targetDate ?? this.targetDate,
+      icon: icon ?? this.icon,
+      colour: colour ?? this.colour,
+      note: note ?? this.note,
+      isCompleted: isCompleted ?? this.isCompleted,
+      isDeleted: isDeleted ?? this.isDeleted,
+      syncStatus: syncStatus ?? this.syncStatus,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
 }
